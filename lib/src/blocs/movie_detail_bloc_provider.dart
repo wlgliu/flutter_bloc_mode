20e3ab9c18924da24d4824795fc3b715ba9a60d6
wangleigang <wangleigang@funji.club>
2020-06-29 07:29:23 +0800

    电影详情

---
+++ lib/src/blocs/movie_detail_bloc_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'movie_detail_bloc.dart';
+export 'movie_detail_bloc.dart';
+
+class MovieDetailBlocProvider extends InheritedWidget {
+  final MovieDetailBloc bloc;
+
+  MovieDetailBlocProvider({Key key, Widget child})
+      : bloc = MovieDetailBloc(),
+        super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(_) {
+    return true;
+  }
+
+  static MovieDetailBloc of(BuildContext context) {
+    // ignore: deprecated_member_use
+    return (context.inheritFromWidgetOfExactType(MovieDetailBlocProvider)as MovieDetailBlocProvider).bloc;
+  }
+}
